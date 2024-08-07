8d504a8a0550296ac382c1cb700dc67c92f68a3e
Shiva Krishna <shivakrishna6032@gmail.com>
2024-08-07 23:27:26 +0530

    ...

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -6,22 +6,8 @@ import { getRecentAppointmentsList } from "@/lib/actions/appointment.actions";
 import {DataTable} from "@/components/table/DataTable";
 import { columns, Payment } from "@/components/table/columns";
 
-async function getData(): Promise<Payment[]> {
-  // Fetch data from your API here.
-  return [
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-    // ...
-  ]
-}
 
 const Admin = async() => {
-
-  const data = await getData()
 
     const appointments = await getRecentAppointmentsList();
 
@@ -70,7 +56,7 @@ const Admin = async() => {
             />
 
         </section>
-            <DataTable data={data} columns={columns} />
+            <DataTable data={appointments.documents} columns={columns} />
 
       </main>
     </div>
